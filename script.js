506600bc47a2ea6a4756d60f38f50591d0f51a59
Jennifer Anderson <jennifer.anderson2425@gmail.com>
2022-09-08 19:01:10 -0400

    fixed git issues

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-//Step 2 added a f(x) to save data
+//added a f(x) to save data
 function saveData()
 {
 
-//Step 1
-//something's wrong with 'name'...keeps saying 'deprecated'???
+//block scope #1 variable below
 let name,email,message;
 
-
+//3 below will retrieve the info you need...GET GET GET!
 name=document.getElementById("name").value;
-email = document.getElementById("email").value;
-message = document.getElementById("message").value;
+email=document.getElementById("email").value;
+message=document.getElementById("message").value;
 
-localStorage.setItem("name", name)
-localStorage.setItem("email", email)
-localStorage.setItem("message", message)
+
+
+
+//I don't want my txt file to have duplicate entries so include array to prevent
+//block scope #2 variable added below as an array
+//https://www.geeksforgeeks.org/how-to-convert-json-string-to-array-of-json-objects-using-javascript/
+let client_responses= Array();
+client_responses=JSON.parse(localStorage.getItem("clients"))?JSON.parse(localStorage.getItem("clients")):[]
+if(client_responses.some((v)=>{return v.email==email}))
+
+{
+    alert("duplicate data");
 }
+else
+{
+    client_responses.push({
+        "name":name,
+        "email":email,
+        "message":message
+    })
+    
+    
+    localStorage.setItem("clients",JSON.stringify(client_responses));
+}
+
+showData();
+
+function showData()
+{
+    //added below line to only allow a new line to be added instead of resending entire batch and then the new message
+    document.getElementById("showCustomers").innerHTML="";
+    let client_responses= Array();
+    client_responses=JSON.parse(localStorage.getItem("clients"))?JSON.parse(localStorage.getItem("clients")):[]
+    if(client_responses)
+    {
+      for(let i=0; i < client_responses.length; i++)  
+      {
+        let addDiv=document.createElement('div');
+        addDiv.className="row";
+        addDiv.innerHTML=' <div class="col-sm-4" style="padding: 10px;">'+client_responses[i].name+'</div><div class="col-sm-4" style="padding: 10px;">'+client_responses[i].email+'</div><div class="col-sm-4" style="padding: 10px;">'+client_responses[i].message+'</div>';
+        document.getElementById("showCustomers").appendChild(addDiv);
+      }
+    }
+
+  
+}
+
+
+}
+
+
