b3b0bbf72d70fdeaeb109c6684a64f6cea913ea4
Jennifer Anderson <jennifer.anderson2425@gmail.com>
2022-09-12 13:16:35 -0400

    nav bar

--- script.js
+++ script.js
@@ -60,4 +60,32 @@ function showData()
 
 }
 
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', ()=> {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.getElementsByClassName.animation) {
+        link.getElementsByClassName.animation = '';
+      } else {
+        link.getElementsByClassName.animation = 'navLinkFade 0.5s ease forwards ${index / 5 + 1}s'; 
+      }  
+});
+
+burger.classList.toggle('toggle');
+
+
+});
+
+
+
+
+}
+
+navSlide();
+
 
